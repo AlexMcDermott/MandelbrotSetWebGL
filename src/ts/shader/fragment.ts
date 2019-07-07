31cadf3d6f6fcc0f92f19b3da912bc591416d9e0
Alex McDermott <alexjmcd2202@gmail.com>
2019-07-08 09:28:11 +1000

    reduced for loop size for mobile

--- src/ts/shader/fragment.ts
+++ src/ts/shader/fragment.ts
@@ -1,5 +1,5 @@
 export default `
-  precision mediump float;
+  precision highp float;
 
   uniform int maxIterations;
   uniform int subSamples;
@@ -9,7 +9,7 @@ export default `
 
   int calcEscapeValue(vec2 c, int maxIterations) {
     vec2 z = vec2(0.0);
-    for (int n = 0; n < 100000; n++) {
+    for (int n = 0; n < 10000; n++) {
       if (n == maxIterations || length(z) > 2.0) return n;
       z = vec2(z.x * z.x - z.y * z.y, z.x * z.y * 2.0) + c;
     }
@@ -20,7 +20,7 @@ export default `
     vec2 c = (gl_FragCoord.xy / resolution) * (maxBounds - minBounds) + minBounds;
 
     int iterationSum = 0;
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 10000; i++) {
       if (i == subSamples) break;
       vec2 subPixel = float(i + 1) * (pixelComplexWidth / float(subSamples + 1)) + c;
       iterationSum += calcEscapeValue(subPixel, maxIterations);
